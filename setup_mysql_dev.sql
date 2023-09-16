7333282cfc4633326e5480ea94c12ae9fd26276e
clairima <mertahrima@gmail.com>
2023-09-16 10:23:32 -0700

    setup/test MYSQL

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,11 @@
+-- Task: Prepare MySQL server for the project
+
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- Create the user if it doesn't exist, and set the password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Grant privileges to the user on the specified databases
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
