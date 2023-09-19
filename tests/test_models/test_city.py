3961c081cde083a4e7bd37d33968c114c042a46f
Omar Khaled <o.khaled6789@gmail.com>
2023-09-20 00:17:41 +0300

    added task 7

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,18 +7,12 @@ from models.city import City
 class test_City(test_basemodel):
     """ testing City class"""
 
-    def __init__(self, *args, **kwargs):
-        """ a constructor method"""
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
-
     def test_state_id(self):
         """ testing state id"""
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        new = City()
+        self.assertTrue(hasattr(new, "state_id"))
 
     def test_name(self):
         """ testing name"""
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        new = City()
+        self.assertTrue(hasattr(new, "name"))
