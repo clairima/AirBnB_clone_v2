991c610452f90ff0596b772acd0880cf4955ce8e
Omar Khaled <o.khaled6789@gmail.com>
2023-09-20 02:27:31 +0300

    completed 7 8 9 and 10 partially

--- models/amenity.py
+++ models/amenity.py
@@ -2,13 +2,15 @@
 """ State Module for HBNB project """
 from sqlalchemy import Column, String, Table
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenity class"""
     __tablename__ = 'amenities'
-
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    place_amenities = relationship(
+        'Place',
+        secondary='place_amenity',
+        back_populates='amenities'
+    )
