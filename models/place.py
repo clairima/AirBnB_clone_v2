d6ff8e648b1399e05a396b217d6cf1deb719dcb2
Omar Khaled <o.khaled6789@gmail.com>
2023-09-21 01:14:24 +0300

    fixed few issues

--- models/place.py
+++ models/place.py
@@ -8,9 +8,10 @@ from sqlalchemy import Table
 from os import getenv
 from models.review import Review
 
+
 place_amenity = Table("place_amenity", Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column("amenity_id", String(60),
                              ForeignKey("amenities.id"),
