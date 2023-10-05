3943ecaef3a9901931d8244d06cd3295daac0342
clairima <mertahrima@gmail.com>
2023-10-05 11:35:20 -0700

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Generates web_static folder.
+"""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """
+    Generate an tgz archive
+    """
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
+    except:
+        return None
